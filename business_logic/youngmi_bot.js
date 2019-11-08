45f05a99d10aecf2799be0cd3ccbc73cba8d8836
Kyungai Lee <klee@iportfolio.co.kr>
2019-11-08 13:38:32 +0900

    월급날과 관련된 기능을 별도의 Obejct(Payday)로 분리하여 리팩토링

--- business_logic/youngmi_bot.js
+++ business_logic/youngmi_bot.js
@@ -1,111 +1,57 @@
 const Helper = require('./helper');
 const ChatbotServer = require('./chatbot_server');
 
-module.exports = class extends ChatbotServer {
+class YoungmiBot extends ChatbotServer {
     constructor() {
         super('iPF_ANNOUNCE_BOT');
     }
 
-    getAvailableDate(date) {
-        while (!Helper.Date.isWorkingDay(date)) {
-            date -= 60 * 60 * 24 * 1000;
+    static getAvailableDate(datetime, find_in_past = true) {
+        let one_day = 60 * 60 * 24 * 1000;
+        if (find_in_past) one_day *= -1;
+
+        while (!Helper.Date.isWorkingDay(datetime)) {
+            datetime += one_day;
         }
 
-        return date;
+        return datetime;
     }
 
-    getPayday(now) {
-        const datetime = new Date(now.getFullYear(), now.getMonth(), 10).getTime();
-        return this.getAvailableDate(datetime);
-    }
-
-    getWorkingDay(since, days_before) {
+    static getWorkingDay(since, days_before) {
         while (days_before > 0) {
             since -= 60 * 60 * 24 * 1000;
 
-            if (since === this.getAvailableDate(since)) days_before--;
+            if (since === YoungmiBot.getAvailableDate(since)) days_before--;
         }
 
         return since;
     }
 
-    getDeadlineForCashDisbursement(time = Date.now()) {
-        const now = new Date(time);
+    static getTheFirstWorkingDayOfMonth(datetime = Date.now()) {
+        const now = new Date(datetime);
 
-        const payday = this.getPayday(now);
-        return this.getWorkingDay(payday, 5);
+        const first_day = new Date(now.getFullYear(), now.getMonth(), 1).getTime();
+
+        return this.getAvailableDate(first_day, false);
     }
 
-    isEndDateForCashDisbursement() {
-        const one_day = (60 * 60 * 24 * 1000);
-        const seven_days = one_day * 7;
-
-        const deadline = this.getDeadlineForCashDisbursement();
-
-        const date_str = [deadline - one_day, deadline - seven_days].map(time => {
-            return new Date(time).toDateString();
-        });
-
-        return date_str.includes(new Date().toDateString());
-    }
+    isTheFirstWorkingDayOfMonth(now = new Date()) {
+        return now.getDate() === new Date(YoungmiBot.getTheFirstWorkingDayOfMonth(now.getTime())).getDate();
+    };
 
     findEvents(forced) {
         let event = [];
 
         if (forced === true) event.push('CASH_DISBURSEMENT');
 
-        if (this.isEndDateForCashDisbursement()) {
+        if (this.isTheFirstWorkingDayOfMonth()) {
             event.push('CASH_DISBURSEMENT');
+
+        } else if (Payday.isDeadlineForTheCashDisbursement()) {
+            event.push('IS_DEADLINE_FOR_CASH_DISBURSEMENT');
         }
 
         return event;
-    }
-
-    generateMessageForCashDisbursement() {
-        const deadline_date = new Date(this.getDeadlineForCashDisbursement());
-        const last_month = new Date(deadline_date.getFullYear(), deadline_date.getMonth(), 0);
-
-        const readable_deadline = `${ (deadline_date.getMonth() + 1) }/${ deadline_date.getDate() }(${ Helper.Date.DAY[deadline_date.getDay()] })`;
-
-        return `*${ last_month.getFullYear() }년 ${ (last_month.getMonth() + 1) }월분 지출결의서 제출하실 분들은 ${ readable_deadline }까지 ymcho에게 제출해 주시기 바랍니다.*
-
-* 제출일정 : ${ readable_deadline }  
-* 그룹리더 서명 받고 제출하기
-1) 기존 지출결의서 양식이 아닌 변경된 양식(링크)을 작성/출력하여
-2) 본인 서명
-3) 소속 그룹 리더의 확인 및 서명을 받고 제출 바랍니다.
-
-${ process.env.CASH_DISBURSEMENT_URL }`;
-    }
-
-    generateMessageForPayday(now = new Date()) {
-        let payday_datetime = this.getPayday(now);
-
-        if (new Date(payday_datetime).getDate() < now.getDate()) {
-            const next_month = new Date(now.getFullYear(), now.getMonth() + 1, 1);
-            payday_datetime = this.getPayday(next_month);
-        }
-
-        let payday = new Date(payday_datetime);
-        const readable_payday = `${ payday.getFullYear() }년 ${ (payday.getMonth() + 1) }월 ${ payday.getDate() }일 ${ Helper.Date.DAY[payday.getDay()]}요일`;
-
-        let message = '';
-
-        const one_day = 60 * 60 * 24 * 1000;
-        payday = new Date(payday_datetime + one_day - 1000); // 23시 59분 59초까지 월급날
-
-        if (payday.getTime() - now.getTime() < one_day) {
-            const party = String.fromCodePoint(0x1F973);
-            message = `바로 오늘!!! 소리질뤄!!!!!!!!! ${ party }`;
-
-        } else {
-            const remain_days = Math.floor((payday.getTime() - now.getTime()) / one_day);
-
-            message = `다음 월급날은 ${ readable_payday }입니다.
-월급날까지 ${ remain_days }일 남았습니다! 힘을 내세여!!!!`;
-        }
-
-        return message;
     }
 
     generateMessage(event) {
@@ -113,7 +59,11 @@ ${ process.env.CASH_DISBURSEMENT_URL }`;
 
         switch (event) {
             case 'CASH_DISBURSEMENT':
-                message = this.generateMessageForCashDisbursement();
+                message = Payday.generateMessageForCashDisbursement();
+                break;
+
+            case 'IS_DEADLINE_FOR_CASH_DISBURSEMENT':
+                message = Payday.generateMessageForCashDisbursementDeadline();
                 break;
 
             default:
@@ -181,7 +131,7 @@ ${ process.env.CASH_DISBURSEMENT_URL }`;
                 response = result ? '공지를 전달했습니다!' : '제가... 뭔가.... 실수를 한 것 같아요. 다시 한 번 메시지 주시겠어요?';
 
             } else if (this.containsSalaryQuestion(user_message)) {
-                response = this.generateMessageForPayday();
+                response = Payday.generateMessageForPayday();
             }
 
             if (for_announce === false) console.log(`[USER_MESSAGE][${ req_body.user.displayName }] ${ user_message }`);
@@ -196,4 +146,81 @@ ${ process.env.CASH_DISBURSEMENT_URL }`;
             };
         }
     }
+}
+
+const Payday = {
+    getPayday(now) {
+        const datetime = new Date(now.getFullYear(), now.getMonth(), 10).getTime();
+        return YoungmiBot.getAvailableDate(datetime);
+    },
+
+    getDeadlineForCashDisbursement(datetime = Date.now()) {
+        const payday = this.getPayday(new Date(datetime));
+        return YoungmiBot.getWorkingDay(payday, 5);
+    },
+
+    isDeadlineForTheCashDisbursement() {
+        const now = new Date();
+        return now.getDate() === new Date(this.getDeadlineForCashDisbursement()).getDate();
+    },
+
+    generateMessageForCashDisbursement() {
+        const deadline_date = new Date(this.getDeadlineForCashDisbursement());
+        const last_month = new Date(deadline_date.getFullYear(), deadline_date.getMonth(), 0);
+
+        const readable_deadline = `${ (deadline_date.getMonth() + 1) }/${ deadline_date.getDate() }(${ Helper.Date.DAY[deadline_date.getDay()] })`;
+
+        return `*${ last_month.getFullYear() }년 ${ (last_month.getMonth() + 1) }월분 지출결의서 제출하실 분들은 ${ readable_deadline }까지 ymcho에게 제출해 주시기 바랍니다.*
+
+* 제출일정 : ${ readable_deadline }  
+* 그룹리더 서명 받고 제출하기
+1) 기존 지출결의서 양식이 아닌 변경된 양식(링크)을 작성/출력하여
+2) 본인 서명
+3) 소속 그룹 리더의 확인 및 서명을 받고 제출 바랍니다.
+
+${ process.env.CASH_DISBURSEMENT_URL }`;
+    },
+
+    generateMessageForCashDisbursementDeadline(now = new Date()) {
+        const last_month = new Date(now.getFullYear(), now.getMonth(), 0);
+
+        return `오늘은 *${ last_month.getFullYear() }년 ${ (last_month.getMonth() + 1) }월분 지출결의서 제출 마감일입니다.`;
+    },
+
+    generateMessageForPayday(now = new Date()) {
+        let payday_datetime = this.getPayday(now);
+
+        if (new Date(payday_datetime).getDate() < now.getDate()) {
+            const next_month = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+            payday_datetime = this.getPayday(next_month);
+        }
+
+        let payday = new Date(payday_datetime);
+        const readable_payday = `${ payday.getFullYear() }년 ${ (payday.getMonth() + 1) }월 ${ payday.getDate() }일 ${ Helper.Date.DAY[payday.getDay()]}요일`;
+
+        let message = '';
+
+        const one_day = 60 * 60 * 24 * 1000;
+        payday = new Date(payday_datetime + one_day - 1000); // 23시 59분 59초까지 월급날
+
+        if (payday.getTime() - now.getTime() < one_day) {
+            const party = String.fromCodePoint(0x1F973);
+            message = `바로 오늘!!! 소리질뤄!!!!!!!!! ${ party }`;
+
+        } else {
+            const remain_days = Math.floor((payday.getTime() - now.getTime()) / one_day);
+
+            message = `다음 월급날은 ${ readable_payday }입니다.
+월급날까지 ${ remain_days }일 남았습니다! 힘을 내세여!!!!`;
+        }
+
+        return message;
+    }
 };
+
+const Announcement = {
+};
+
+module.exports.YoungmiBot = YoungmiBot;
+
+module.exports.Payday = Payday;
--- test/youngmi_bot.js
+++ test/youngmi_bot.js
@@ -4,40 +4,73 @@ const chai = require('chai');
 const expect = chai.expect;
 const Util = require('../business_logic/util');
 const Helper = require('../business_logic/helper');
-const YoungmiBot = require('../business_logic/youngmi_bot');
+const { YoungmiBot, Payday } = require('../business_logic/youngmi_bot');
 
 const bot = new YoungmiBot();
 
-describe('date', function() {
-    it ('payday is weekday', () => {
-        const test = new Date('1 December, 2019').getTime();
-        const target_date = bot.getDeadlineForCashDisbursement(test);
+describe('working day', function() {
+    it('get the first working date', () => {
+        const first_day = YoungmiBot.getTheFirstWorkingDayOfMonth(new Date('10 November, 2019').getTime());
+        const correct = Util.convertUTCTimeToLocalTime(new Date(first_day).toISOString()).startsWith('11/1/2019');
 
-        const deadline = Util.convertUTCTimeToLocalTime(target_date);
+        expect(correct).to.be.true;
     });
 
-    it ('payday is weekend', () => {
-        const test = new Date('1 November, 2019').getTime();
-        const target_date = bot.getDeadlineForCashDisbursement(test);
+    it('is the first working day', () => {
+        const first_day = new Date('2 December, 2019');
 
-        const deadline = Util.convertUTCTimeToLocalTime(target_date);
+        expect(bot.isTheFirstWorkingDayOfMonth(first_day)).to.be.true;
     });
 
-    it ('payday has a year gap', () => {
-        const test = new Date('1 January, 2020').getTime();
-        const target_date = bot.getDeadlineForCashDisbursement(test);
+    it('is not the first working day', () => {
+        const first_day = new Date('1 December, 2019');
 
-        const deadline = Util.convertUTCTimeToLocalTime(target_date);
+        expect(bot.isTheFirstWorkingDayOfMonth(first_day)).to.be.false;
+    });
+});
+
+describe('payday', function() {
+    it ('deadline (payday is weekday)', () => {
+        const now = new Date('1 December, 2019').getTime();
+        const deadline = Payday.getDeadlineForCashDisbursement(now);
+
+        // correct deadline 2019/12/3
+        expect(new Date(deadline).getDate()).to.be.equal(3);
+    });
+
+    it ('deadline (payday is not working day)', () => {
+        const now = new Date('1 November, 2019').getTime();
+        const deadline = Payday.getDeadlineForCashDisbursement(now);
+
+        // correct deadline 2019/11/1
+        expect(new Date(deadline).getDate()).to.be.equal(1);
+    });
+
+    it ('payday (payday is weekday)', () => {
+        const now = new Date('1 January, 2020');
+        const payday = Payday.getPayday(now);
+
+        // correct payday 2020/1/10
+        expect(new Date(payday).getDate()).to.be.equal(10);
+    });
+
+    it('payday (payday is not working day', () => {
+        const now = new Date('2 November, 2019');
+        const payday = Payday.getPayday(now);
+
+        // correct payday 2010/11/8
+        expect(new Date(payday).getDate()).to.be.equal(8);
     });
 });
 
 describe('message', function() {
     it('correct message', () => {
-        const message = bot.generateMessageForCashDisbursement();
+        const message = Payday.generateMessageForCashDisbursement();
         console.log(message);
     })
 });
 
+/*/
 describe('send', function() {
     it('send message', async () => {
         process.env.GOOGLE_APPLICATION_CREDENTIALS = process.cwd() + '/auth.json';
@@ -45,3 +78,4 @@ describe('send', function() {
         await bot.sendNotifications();
     });
 });
+/**/
